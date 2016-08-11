1cb61db6e32756c5c9e7459f49deea26632fc949
clerk67 <clerk67@gmail.com>
2016-08-12 03:52:27 +0900

    Updated ppt2rst.rb

--- ppt2rst.rb
+++ ppt2rst.rb
@@ -9,6 +9,77 @@ TEMP_DIR = ".ppt2rst_tmp/"
 TITLELINE_LENGTH = 80
 PROGRESSBAR_LENGTH = 63
 
+def p2rst(p, heading)
+	return "" if p.elements["a:r"].nil?
+	pPr = p.elements["a:pPr"]
+	bullet = "* "
+	level = 0
+	unless pPr.nil?
+		bullet = "" unless pPr.elements["a:buNone"].nil?
+		bullet = "#. " unless pPr.elements["a:buAutoNum"].nil?
+		lvl = pPr.attributes["lvl"]
+		level = lvl.to_i unless lvl.nil?
+	end
+	rst = ""
+	rst << "\t" * level + bullet if heading == 0
+	p.elements.each("a:r") do |r|
+		rst << r2rst(r)
+	end
+	rst << "\n" if heading == 0
+	rst
+end
+
+def r2rst(r)
+	rPr = r.elements["a:rPr"]
+	if rPr&.attributes["b"] == "1"
+		astah = 2
+	elsif rPr&.attributes["i"] == "1"
+		astah = 1
+	else
+		astah = 0
+	end
+	rst = ""
+	t = r.elements["a:t"]
+	unless t&.text.to_s.empty?
+		text = t.text.to_s.gsub(/\*/, "\\*").gsub(/\#/, "\\#")
+		rst << "\\ " unless astah == 0 || rst.size == 0 || rst[-1] == " "
+		rst << "*" * astah
+		if astah == 0
+			rst << text
+		else
+			rst << text.strip
+		end
+		rst << "*" * astah
+		rst << " " unless astah == 0
+	end
+	rst
+end
+
+def tbl2rst(tbl, header_rows, stub_columns)
+	rst = ".. list-table:: \\ \n"
+	rst << "   :header-rows: #{header_rows}\n"
+	rst << "   :stub-columns: #{stub_columns}\n\n"
+	tbl.elements.each("a:tr") do |tr|
+		rst << "   *"
+		tr.elements.each("a:tc") do |tc|
+			rst << "    " unless rst[-1] == "*"
+			rst << " - "
+			tc.elements.each("a:txBody/a:p/a:r") do |r|
+				rst << r2rst(r)
+			end
+			rst << "\n"
+		end
+	end
+	rst << "\n"
+	rst
+end
+
+def show_progress(value, max)
+	printf("\r[%3d/%3d] |", value, max)
+	print "=" * (PROGRESSBAR_LENGTH * value / max) + " " * (PROGRESSBAR_LENGTH - (PROGRESSBAR_LENGTH * value / max))
+	printf("| %3d\%", 100 * value / max)
+end
+
 unless ARGV.size == 2
 	puts "ERROR: Invalid arguments"
 	exit
@@ -26,17 +97,13 @@ Zip::File.open("#{ARGV[0]}") do |zip|
 	count = 0
 	zip.each do |entry|
 		dirname = File::dirname(entry.to_s)
-		if dirname == SLIDES_DIR
-			count += 1
-		end
+		count += 1 if dirname == SLIDES_DIR
 	end
-	i = 1
+	i = 0
 	zip.each do |entry|
 		dirname = File::dirname(entry.to_s)
 		if dirname == SLIDES_DIR
-			printf("\r[%3d/%3d] |", i, count)
-			print "=" * (PROGRESSBAR_LENGTH * i / count) + " " * (PROGRESSBAR_LENGTH - (PROGRESSBAR_LENGTH * i / count))
-			printf("| %3d\%", 100 * i / count)
+			show_progress(i + 1, count)
 			zip.extract(entry, TEMP_DIR + entry.to_s) {true}
 			i += 1
 		end
@@ -46,30 +113,34 @@ end
 count = 0
 ls = Dir::entries(TEMP_DIR + SLIDES_DIR)
 ls.each do |line|
-	if line.match(/slide\d+\.xml/)
-		count += 1
-	end
+	count += 1 if line.match(/slide\d+\.xml/)
 end
 
-i = 1
 rst = ""
 puts "\nGenerating reStructuredText..."
 
-while File.exist?(TEMP_DIR + SLIDES_DIR + "/slide" + i.to_s + ".xml")
-	printf("\r[%3d/%3d] |", i, count)
-	print "=" * (PROGRESSBAR_LENGTH * i / count) + " " * (PROGRESSBAR_LENGTH - (PROGRESSBAR_LENGTH * i / count))
-	printf("| %3d\%", 100 * i / count)
-	xml = REXML::Document.new(open(TEMP_DIR + SLIDES_DIR + "/slide" + i.to_s + ".xml"))
-	rst << ".. slide #{i}/#{count}\n\n"
+count.times do |i|
+	show_progress(i + 1, count)
+	xml = REXML::Document.new(open(TEMP_DIR + SLIDES_DIR + "/slide" + (i + 1).to_s + ".xml"))
+	rst << ".. slide #{i + 1}/#{count}\n\n"
 	spTree = xml.elements["p:sld/p:cSld/p:spTree"]
 	spTree.elements.each do |element|
 		case element.name
 		when "sp"
 			sp = element
-			ph = sp.elements["p:nvSpPr/p:nvPr/p:ph"]
+			nvSpPr = sp.elements["p:nvSpPr"]
+			cNvSpPr = nvSpPr.elements["p:cNvSpPr"]
 			heading = 0
+			txBox = false
+			if cNvSpPr&.attributes["txBox"] == "1"
+				heading = -1
+				txBox = true
+			end
+			ph = nvSpPr.elements["p:nvPr/p:ph"]
 			unless ph.nil?
 				case ph.attributes["type"]
+				when "ctrTitle"
+					heading = 1
 				when "title"
 					heading = 3
 				when "subTitle"
@@ -91,57 +162,7 @@ while File.exist?(TEMP_DIR + SLIDES_DIR + "/slide" + i.to_s + ".xml")
 				rst << "-" * TITLELINE_LENGTH + "\n"
 			end
 			sp.elements.each("p:txBody/a:p") do |p|
-				pPr = p.elements["a:pPr"]
-				bullet = "* "
-				level = 0
-				unless pPr.nil?
-					unless pPr.elements["a:buNone"].nil?
-						bullet = ""
-					else
-						unless pPr.elements["a:buAutoNum"].nil?
-							bullet = "#. "
-						end
-						lvl = pPr.attributes["lvl"]
-						unless lvl.nil?
-							level = lvl.to_i
-						end
-					end
-				end
-				if heading == 0
-					rst << "\t" * level + bullet
-				end
-				p.elements.each("a:r") do |r|
-					rPr = r.elements["a:rPr"]
-					astah = 0
-					unless rPr.nil?
-						if rPr.attributes["i"] == "1"
-							astah += 1
-						end
-						if rPr.attributes["b"] == "1"
-							astah += 2
-						end
-					end
-					t = r.elements["a:t"]
-					unless t.nil? || t.text.to_s.empty?
-						text = t.text.to_s.gsub(/\*/, "\\*").gsub(/\#/, "\\#")
-						unless astah == 0 || rst[-1] == " "
-							rst << " "
-						end
-						rst << "*" * astah
-						if astah == 0
-							rst << text
-						else
-							rst << text.strip
-						end
-						rst << "*" * astah
-						unless astah == 0
-							rst << " "
-						end
-					end
-				end
-				if heading == 0
-					rst << "\n"
-				end
+				rst << p2rst(p, heading)
 			end
 			rst << "\n"
 			case heading
@@ -153,29 +174,12 @@ while File.exist?(TEMP_DIR + SLIDES_DIR + "/slide" + i.to_s + ".xml")
 		when "graphicFrame"
 			graphicFrame = element
 			tbl = graphicFrame.elements["a:graphic/a:graphicData/a:tbl"]
-			unless tbl.nil?
-				rst << ".. list-table::\n\n"
-				tbl.elements.each("a:tr") do |tr|
-					rst << "\t*"
-					tr.elements.each("a:tc") do |tc|
-						if rst[-1] == "*"
-							rst << " - "
-						else
-							rst << "\t  - "
-						end
-						tc.elements.each("a:txBody/a:p/a:r/a:t") do |t|
-							rst << t.text.to_s.gsub(/\*/, "\\*").gsub(/\#/, "\\#")
-						end
-						rst << "\n"
-					end
-				end
-				rst << "\n"
-			end
+			rst << tbl2rst(tbl, 0, 0) unless tbl.nil?
 		end
 	end
-	i += 1
 end
 
 File.write("#{ARGV[1]}", rst)
+FileUtils.rm_rf(TEMP_DIR)
 puts "\nSUCCESS: #{rst.count("\n")} lines were written to #{ARGV[1]}"
 
